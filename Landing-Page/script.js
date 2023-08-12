f2d544bde68c63e3697ef1c450e075f74e595aaf
Camilo Jose Martinez <cajomapa@gmail.com>
2023-08-11 23:04:37 -0500

    quiero dormir

---
+++ Landing-Page/script.js
@@ -0,0 +1,37 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountainsFront = document.getElementById('mountains_front');
+let mountainsBehind = document.getElementById('mountains_behind');
+let titulo = document.getElementById('titulo');
+let margen = document.querySelector('.margen');
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+
+    // Calculate the desired position for the 'titulo' element
+    let desiredHorizontalPosition = (window.innerWidth - titulo.offsetWidth) / 2;
+
+
+    // Calculate the maximum margin-right value to stop horizontally
+    let maxMarginRight = desiredHorizontalPosition + 350; // Considering initial right: -350px
+
+    // Calculate the new margin-right value based on scroll position
+    let newMarginRight = Math.min(maxMarginRight, value * 4);
+
+
+
+    // Adjust the margin-right and top of the 'titulo' element
+    titulo.style.marginRight = newMarginRight + 'px';
+
+
+    // Adjust other animations as you did before
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountainsFront.style.top = value * 0.3 + 'px';
+    mountainsBehind.style.top = value * 0.5 + 'px';
+});
+
+
+
+
+
