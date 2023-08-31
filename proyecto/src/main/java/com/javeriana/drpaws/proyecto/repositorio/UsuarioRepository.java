d64cc1e04e901ee6af42add399c0f0155e09e383
Betico1928 <betoveluis@gmail.com>
2023-08-31 02:13:08 -0500

    Cambios a la base de datos.

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/UsuarioRepository.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/UsuarioRepository.java
@@ -18,10 +18,10 @@ public class UsuarioRepository {
 
     public UsuarioRepository() {
         usuarios.put(1,new Usuario(1,"Alejandro Sanz", "alejandro-sanz@gmail.com","+34 612-34-56-78","contraseña", new ArrayList<>(List.of(1,2)),"alejandro_sanz.jpg"));
-                    usuarios.put(2,new Usuario(2,"Daniela", "daniela@gmail.com","333-455-5685","contraseña", new ArrayList<>(
-            List.of(2,3)),"alejandro_sanz.jpg"));
-                    usuarios.put(3,new Usuario(3,"Santiago", "erick@gmail.com","234-344-2342","contraseña", new ArrayList<>(
-            List.of(4,1)),"alejandro_sanz.jpg"));
+                    usuarios.put(2,new Usuario(2,"Adele Laurie Blue Adkins", "adele@gmail.com","+44 2071-234-567","contraseña", new ArrayList<>(
+            List.of(2,3)),"adele.jpg"));
+                    usuarios.put(3,new Usuario(3,"Daft Punk", "daft-punk-jeje@gmail.com","+33 1-23-45-67-89","contraseña", new ArrayList<>(
+            List.of(4,1)),"daft_punk.jpg"));
     }
 
     public Usuario findById(int id) {
