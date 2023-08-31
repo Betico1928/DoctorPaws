21281fdd77a7786ec54b16968cf62e2ff7e914f5
Betico1928 <betoveluis@gmail.com>
2023-08-31 12:15:55 -0500

    Cambios a las imagenes de las mascotas
    TODO: Que sale el PATH de la BD.

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/MascotaRepository.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/MascotaRepository.java
@@ -15,13 +15,13 @@ public class MascotaRepository {
 
     public MascotaRepository() {
         mascotas.put(1, new Mascota(1, "Toby", "Dachshund (Salchicha)", 4, 20.5f, " La propietaria trajo a Toby después de encontrarlo repetidamente 'escondido' con solo su cabeza bajo el sofá y el resto de su largo cuerpo claramente visible. A pesar de los repetidos intentos de mostrarle cómo esconder todo su cuerpo, Toby insistía en que su método era el mejor.",
-                "samoyed"));
+                "toby.jpg"));
         mascotas.put(2, new Mascota(2, "Bella", "Bulldog Francés", 2, 36.5f, "Bella ha comenzado a roncar tan fuerte que incluso despertó a los vecinos a través de las paredes del departamento. La dueña sospecha que Bella podría estar tratando de competir con el ronquido del abuelo.",
-                "beagle"));
+                "Bella.png"));
         mascotas.put(3, new Mascota(3, "Sparky", "Border Collie", 7, 30.5f, "Sparky ha estado persiguiendo sombras por toda la casa, especialmente las que genera el movimiento de las aspas del ventilador de techo. Recientemente, la propietaria encontró a Sparky sentado en el centro de la sala, mirando fijamente la lámpara durante horas, esperando que se moviera.",
-                "pug"));
+                "Sparky.jpg"));
         mascotas.put(4, new Mascota(4, "Lulu", "Chihuahua", 3, 6.5f, "Cada vez que alguien intenta ponerse zapatos en la casa, Lulu se interpone, ladrando y defendiendo los zapatos como si fueran suyos. Incluso se ha observado que duerme dentro de ellos.",
-                "shiba"));
+                "Lulu.png"));
     }
 
     public Mascota findById(int id) {
