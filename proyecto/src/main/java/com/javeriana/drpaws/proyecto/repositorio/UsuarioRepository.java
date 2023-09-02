644e53b077cfd077a3abf65f3a89b8ccb0d75ca9
Camilo Jose Martinez <cajomapa@gmail.com>
2023-09-01 20:48:38 -0500

    remove unused imports

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/UsuarioRepository.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/repositorio/UsuarioRepository.java
@@ -1,11 +1,6 @@
 package com.javeriana.drpaws.proyecto.repositorio;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
