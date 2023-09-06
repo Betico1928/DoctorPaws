22101c1cdc1a4e4e5982029441f7dfb984a9a718
Betico1928 <betoveluis@gmail.com>
2023-09-06 18:04:22 -0500

    Página de testimonios parcialmente completada.
    Se hizo una parte de la página donde estarán los testimonios y también se actualizaron todos los links del footer para que estos se encuentren conectados con las demás páginas de Dr. Paws.

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/controlador/PorqueNosotrosController.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/controlador/PorqueNosotrosController.java
@@ -11,4 +11,10 @@ public class PorqueNosotrosController
     {
         return "porque-nosotros";
     }
+
+    @RequestMapping("/porqueNosotros/testimonios")
+    public String showTestimoniosPage()
+    {
+        return "testimonios";
+    }
 }
