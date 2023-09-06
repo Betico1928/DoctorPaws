1800849d0cdc86f50b1e3cacba514dd1f1124985
Camilo Jose Martinez <cajomapa@gmail.com>
2023-09-06 18:50:10 -0500

    Limpieza de codigo

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/entidad/Veterinario.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/entidad/Veterinario.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 
 @Entity
 public class Veterinario {
