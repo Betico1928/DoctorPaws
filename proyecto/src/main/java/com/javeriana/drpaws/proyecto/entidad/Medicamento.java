a639701b0e296bb1329ad8a49603596ff928b1e7
Betico1928 <betoveluis@gmail.com>
2023-10-04 22:05:44 -0500

    Cambios en la entidad de Medicamento.
    Se hicieron cambios en la entidad de medicamentos para poder agregarlos a la base de datos teniendo en cuenta datos requeridos por la veterinaria. Así mismo se organizó el DataBaseInit, se cargó el nuevo enunciado y se subió el Excel suministrado por veterinaria con todos los datos de los medicamentos.

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/entidad/Medicamento.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/entidad/Medicamento.java
@@ -7,7 +7,6 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
@@ -17,51 +16,97 @@ public class Medicamento {
     private Long id;
 
     private String nombre;
-    private float precio;
+    private float precioVenta;
+    private float precioCompra;
+    private int unidadesDisponibles;
+    private int unidadesVendidas;
+
 
     @OneToMany(mappedBy = "medicamento")
     private List<Tratamiento> tratamientos = new ArrayList<>();
 
-    public Medicamento(String nombre, float precio) {
+    // Constructor
+    public Medicamento(String nombre, float precioVenta, float precioCompra, int unidadesDisponibles, int unidadesVendidas) {
         this.nombre = nombre;
-        this.precio = precio;
+        this.precioVenta = precioVenta;
+        this.precioCompra = precioCompra;
+        this.unidadesDisponibles = unidadesDisponibles;
+        this.unidadesVendidas = unidadesVendidas;
     }
 
-    public Medicamento() {
-
-    }
+    // Constructor Vacío
+    public Medicamento() {}
+    
 
     // Getters y Setters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public float getPrecio() {
-        return precio;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setPrecio(float precio) {
-        this.precio = precio;
-    }
-
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
-    public List<Tratamiento> getTratamientos() {
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public float getPrecioVenta()
+    {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(float precioVenta)
+    {
+        this.precioVenta = precioVenta;
+    }
+
+    public float getPrecioCompra()
+    {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(float precioCompra)
+    {
+        this.precioCompra = precioCompra;
+    }
+
+    public int getUnidadesDisponibles()
+    {
+        return unidadesDisponibles;
+    }
+
+    public void setUnidadesDisponibles(int unidadesDisponibles)
+    {
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+
+    public int getUnidadesVendidas()
+    {
+        return unidadesVendidas;
+    }
+
+    public void setUnidadesVendidas(int unidadesVendidas)
+    {
+        this.unidadesVendidas = unidadesVendidas;
+    }
+
+
+    public List<Tratamiento> getTratamientos()
+    {
         return tratamientos;
     }
 
-    public void setTratamientos(List<Tratamiento> tratamientos) {
+    public void setTratamientos(List<Tratamiento> tratamientos)
+    {
         this.tratamientos = tratamientos;
     }
-
 }
