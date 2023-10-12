be0849b0b2693e30265bf78c2405a459c50b4139
Camilo Jose Martinez <cajomapa@gmail.com>
2023-10-12 01:33:16 -0500

    Cross origin

--- proyecto/src/main/java/com/javeriana/drpaws/proyecto/controlador/VeterinarioController.java
+++ proyecto/src/main/java/com/javeriana/drpaws/proyecto/controlador/VeterinarioController.java
@@ -2,6 +2,7 @@ package com.javeriana.drpaws.proyecto.controlador;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.javeriana.drpaws.proyecto.servicio.Veterinario.VeterinarioService;
 
 @RestController
 @RequestMapping("/veterinarios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class VeterinarioController {
 
     private final VeterinarioService veterinarioService;
