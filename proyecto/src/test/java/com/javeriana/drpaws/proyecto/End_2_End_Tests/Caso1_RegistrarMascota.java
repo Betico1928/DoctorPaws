ae0acec907d70e2f1ec4d164e3ef6b54d1cd56d8
Alberto Vigna <betoveluis@gmail.com>
2023-10-25 20:20:03 -0500

    Añadidas las opciones para el test de registrar mascota.

--- proyecto/src/test/java/com/javeriana/drpaws/proyecto/End_2_End_Tests/Caso1_RegistrarMascota.java
+++ proyecto/src/test/java/com/javeriana/drpaws/proyecto/End_2_End_Tests/Caso1_RegistrarMascota.java
@@ -1,14 +1,38 @@
 package com.javeriana.drpaws.proyecto.End_2_End_Tests;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Duration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class Caso1_RegistrarMascota
 {
-    
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        //options.addArguments("--headless");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-notifications");
+
+        this.driver = new ChromeDriver(options);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
 }
