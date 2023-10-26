066d438fffdca92044163fbd7df691d9365e6269
Alberto Vigna <betoveluis@gmail.com>
2023-10-26 01:11:48 -0500

    Optimización de Imports

--- proyecto/src/test/java/com/javeriana/drpaws/proyecto/End_2_End_Tests/Caso1_RegistrarMascota.java
+++ proyecto/src/test/java/com/javeriana/drpaws/proyecto/End_2_End_Tests/Caso1_RegistrarMascota.java
@@ -8,18 +8,15 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Duration;
-import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
