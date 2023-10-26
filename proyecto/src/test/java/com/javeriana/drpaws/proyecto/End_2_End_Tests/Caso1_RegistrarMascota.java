e698dc43a7a3af3cf847d138539570683f785b96
Alberto Vigna <betoveluis@gmail.com>
2023-10-25 20:06:20 -0500

    Creada la prueba para el Caso #1 - Registrar Mascota.

---
+++ proyecto/src/test/java/com/javeriana/drpaws/proyecto/End_2_End_Tests/Caso1_RegistrarMascota.java
@@ -0,0 +1,14 @@
+package com.javeriana.drpaws.proyecto.End_2_End_Tests;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class Caso1_RegistrarMascota
+{
+    
+}
